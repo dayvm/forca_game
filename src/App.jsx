2bd61bbe3a6ce1f9aeadb27cb9e9ac9525938272
setbr1 <dcdm2mil@gmail.com>
2025-09-17 18:26:03 -0300

    feat: componente Header na tela e preparação dos demais componentes e dados das palavras

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
-      <div>Oi</div>
+      <main className="container">
+        <Header />
+      </main>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Forca Game</h1>
+            <button>Reiniciar</button>
+        </header>
+    );
+}
