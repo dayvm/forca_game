849bc3878087e50810ca9f3a77ba9e81a3973dbd
setbr1 <dcdm2mil@gmail.com>
2025-09-24 21:07:04 -0300

    chore: botoes iniciar/reiniciar estilizacao

--- src/components/BotaoJogar.jsx
+++ src/components/BotaoJogar.jsx
@@ -2,9 +2,9 @@ export default function BotaoJogar({ iniciarJogo, statusDoJogo }) {
     return (
         <div>
             {statusDoJogo==="home" ? (
-                <button onClick={iniciarJogo}>Iniciar</button>
+                <button className= "botao-jogar" onClick={iniciarJogo}>Iniciar</button>
             ) : (
-                <button onClick={iniciarJogo}>Reiniciar</button>
+                <button className= "botao-jogar" onClick={iniciarJogo}>Reiniciar</button>
             )}
         </div>
     );
